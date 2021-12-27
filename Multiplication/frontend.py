7eece5d5ff1b59e17a94af5d77071eae6cc5ed73
Vaas <geniatullin.marat@gmail.com>
2021-12-27 22:26:31 +0300

    init

--- Multiplication/frontend.py
+++ Multiplication/frontend.py
@@ -1,5 +1,21 @@
 from tkinter import *
 
+def multiplication():
+    try:
+        num = number_user.get()
+        num = int(num)
+    except ValueError:
+        number_user.delete(0, END)
+        return
+    for i in range(1, 10):
+        total = i * num
+        write = str(i) + '*' + str(num) + '=' + str(total)
+        multi_list.insert(END, write)
+
+def clear_all_fields():
+    number_user.delete(0, END)
+    multi_list.delete(0, END)
+
 window = Tk()
 window.title('Times Table')
 window.geometry('400x235')
@@ -13,10 +29,10 @@ number_user.place(x=125, y=20, width=120, height=25)
 multi_list = Listbox()
 multi_list.place(x=125, y=60, width=120, height=150)
 
-button_table = Button(text='View Times Table')
+button_table = Button(text='View Times Table', command=multiplication)
 button_table.place(x=260, y=20, width=120, height=25)
 
-button_clear = Button(text='Clear')
+button_clear = Button(text='Clear', command=clear_all_fields)
 button_clear.place(x=260, y=60, width=120, height=25)
 
 if __name__ == '__main__':
