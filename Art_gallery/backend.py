3f91c55896cf9eb36908ae1d981bbc3763d7d78f
Vaas <geniatullin.marat@gmail.com>
2021-12-28 18:35:46 +0300

    add argument 'sql_request' for def last_id add LableFrame for "Add Artist Group"

--- Art_gallery/backend.py
+++ Art_gallery/backend.py
@@ -1,10 +1,10 @@
 import sqlite3
 from tkinter import *
 
-def last_id(base_data: str):
+def last_id(base_data: str, sql_request: str):
     db = sqlite3.connect(base_data)
     cursor = db.cursor()
-    cursor.execute("SELECT ArtistsID FROM Artists")
+    cursor.execute(sql_request)
     list_id = []
     for i in cursor.fetchall():
         list_id.append(i[0])
@@ -15,7 +15,7 @@ def last_id(base_data: str):
 def add_artist(base_data: str):
     db = sqlite3.connect(base_data)
     cursor = db.cursor()
-    id_set = last_id(base_data) + 1
+    id_set = last_id(base_data, ARTIST_ID) + 1
     name_set = name_artist.get()
     address_set = address_artist.get()
     town_set = town_artist.get()
@@ -31,42 +31,51 @@ def add_artist(base_data: str):
     db.commit()
     db.close()
 
+ARTIST_ID = "SELECT ArtistsID FROM Artists"
+PIECE_ID = "SELECT PieceID FROM Pictures"
+
 window = Tk()
 window.title('Art gallery base')
 window.geometry('720x600')
 
+#Group Add Artist
+frame_artist = LabelFrame(text='Add artist')
+frame_artist.place(x=10, y=10, height=110, width=700)
+
 name_lable = Label(text='Name')
-name_lable.place(x=20, y=20)
+name_lable.place(x=20, y=30)
 
 address_lable = Label(text='Address')
-address_lable.place(x=160, y=20)
+address_lable.place(x=160, y=30)
 
 town_lable = Label(text='Town')
-town_lable.place(x=300, y=20)
+town_lable.place(x=300, y=30)
 
 country_lable = Label(text='Country')
-country_lable.place(x=440, y=20)
+country_lable.place(x=440, y=30)
 
 postcode_lable = Label(text='Postcode')
-postcode_lable.place(x=580, y=20)
+postcode_lable.place(x=580, y=30)
 
 name_artist = Entry(text='')
-name_artist.place(x=20, y=40, width=120)
+name_artist.place(x=20, y=50, width=120)
 
 address_artist = Entry(text='')
-address_artist.place(x=160, y=40, width=120)
+address_artist.place(x=160, y=50, width=120)
 
 town_artist = Entry(text='')
-town_artist.place(x=300, y=40, width=120)
+town_artist.place(x=300, y=50, width=120)
 
 country_artist = Entry(text='')
-country_artist.place(x=440, y=40, width=120)
+country_artist.place(x=440, y=50, width=120)
 
 postcode_artist = Entry(text='')
-postcode_artist.place(x=580, y=40, width=120)
+postcode_artist.place(x=580, y=50, width=120)
 
 button_add_artist = Button(text='Add artist', command=lambda: add_artist('ArtGallery.db'))
-button_add_artist.place(x=580, y=70, width=120)
+button_add_artist.place(x=580, y=80, width=120)
+
+#Group Add Picture
 
 if __name__ == '__main__':
     window.mainloop()
